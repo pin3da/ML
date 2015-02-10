5354b7944cd326f984e6073d775bda1ea5e083d6
Alejandro <ing.alsuga@gmail.com>
2015-02-09 21:54:57 -0500

    test_optimizer working

--- random/test_optimizer.cc
+++ random/test_optimizer.cc
@@ -17,33 +17,33 @@ class OptimizerTestFunction {
     double x = coordinates(0, 0);
     return 5.0 - (x - 3.0) * (x - 3.0);
   }
-  
+
   void Gradient(const arma::mat& coordinates, arma::mat& gradient) {
     double x = coordinates(0, 0);
     gradient.set_size(1, 1);
-    gradient(0, 0) = -6.0 * (x - 3.0);
+    gradient(0, 0) = - 2.0 * (x - 3.0);
   }
 
-  size_t NumConstraints() const { 
+  size_t NumConstraints() const {
     return 1;
   }
 
   double EvaluateConstraint(const size_t index, const arma::mat& coordinates) {
     if (index != 0)
       return 0;
-    
-    double x = coordinates(0, 0);
+
+    double x = min(0.0, coordinates(0, 0) - 2.0);
     return x;
   }
-  
+
   void GradientConstraint(const size_t index, const arma::mat& coordinates, arma::mat& gradient) {
-     if (index == 0) {
+    if (index == 0) {
       gradient.ones(1, 1);
     }
   }
 
-  const arma::mat& GetInitialPoint() const { 
-    return initialPoint; 
+  const arma::mat& GetInitialPoint() const {
+    return initialPoint;
   }
 
   // convert the obkect into a string
@@ -60,13 +60,13 @@ int main() {
   OptimizerTestFunction func;
   AugLagrangian<OptimizerTestFunction> aug(func);
   arma::vec coords = func.GetInitialPoint();
-  if (!aug.Optimize(coords, 0)) {
+  if (aug.Optimize(coords, 0)) {
     double finalValue = func.Evaluate(coords);
     coords.print();
     cout << finalValue << endl;
   } else {
     cout << "something went wrong" << endl;
   }
-  
+
   return 0;
 }
